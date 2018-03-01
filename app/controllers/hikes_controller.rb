9388b3db171b9703309cc1033a621d111d85a0f8
Lindsay Block <lindsay.block05@gmail.com>
2018-02-28 22:02:50 -0700

    adding edit pages

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -21,8 +21,21 @@ class HikesController < ApplicationController
     end
   end
 
+  def edit
+    @hike = Hike.find(params[:id])
+  end
+
+  def update
+    @hike = Hike.find(params[:id])
+    if @hike.update(hike_params)
+      redirect_to hikes_path
+    else
+      render :edit
+    end
+  end 
+
   private
-  
+
   def hike_params
     params.require(:hike).permit(:name, :location)
   end
